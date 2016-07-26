54b249c9d3535f9020fe509eddb284548d24d5b5
Elikem Adadevoh <elikem@gmail.com>
2016-07-26 18:48:21 -0400

    Clean up sign-in page, remove give.css, update typekit

--- _includes/header.php
+++ _includes/header.php
@@ -7,7 +7,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="/assets/css/give.css">
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
     
---
+++ sign-in/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('../_functions/function.php');
+
+give_head();
+
+?>
+
+<div class="screenContent" tabindex="-1" role="dialog">
+    <div class="container">
+        <div class="col-md-12 col-xs-12">
+            <div class="panel sign-in-panel">
+                <div class="panel-body">
+                    <div class="row vdivide">
+                        <div class="col-md-6 sign-in-section">
+                            <h3 class="text-center">Sign In</h3>
+                            <p class="text-center section-description">Logging in saves you time during checkout.</p>
+
+                            <form>
+                                <div class="form-group">
+                                    <label for="username">USERNAME: <span class="text-capitalize">(Your Email)</span></label>
+                                    <input type="text" class="form-control form-control-subtle" placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">PASSWORD:</label>
+                                    <input type="password" class="form-control form-control-subtle" placeholder="Password">
+                                </div>
+                                <button type="submit" class="btn btn-block btn-primary">Sign In</button>
+                            </form>
+
+                            <div class="text-center self-service">Forgot your password? <a href="">Reset It</a></div>
+                        </div>
+
+                        <div class="col-md-6 sign-in-section">
+                            <h3 class="text-center">Checkout as Guest</h3>
+                            <p class="text-center section-description">You will have an opportunity to register after completing your transaction.</p>
+
+                            <button type="submit" class="btn btn-block btn-primary">Continue as Guest</button>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- // panel -->
+        </div>
+    </div>
+</div>
+
+<?php
+
+give_footer();
+
+?>
