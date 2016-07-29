c60c52373cebc4e69563e4c1e9b511df267eb896
David Sudarma <david@26am.com>
2016-07-29 07:31:20 -0700

    Added modal sequence for Reset Password

--- _modals/modal-sign-in-recognized.php
+++ _modals/modal-sign-in-recognized.php
@@ -8,7 +8,7 @@
       <div class="modal-body">
         <div class="container-fluid" tabindex="-1" role="dialog">
           <div class="col-md-12 col-xs-12">
-            <p class="text-center mb0"><strong>Welcome back, Hezekiah!</strong></p>
+            <p class="text-center mb0"><strong>Welcome back, Micah Sudarma!</strong></p>
             <form>
               <div class="form-group">
                 <label for="email">Email</label>
@@ -20,7 +20,7 @@
               </div>
               <button type="submit" class="btn btn-block btn-primary">Sign In</button>
             </form>
-            <p class="text-center mt mb0">Forgot your password? <a href="">Reset It</a></p>
+            <p class="text-center mt mb0">Forgot your password? <a data-toggle="modal" data-dismiss="modal" data-target="#resetModal" >Reset It</a></p>
             <p class="text-center">Not Hezekiah? <a href="">Sign Out</a></p>
           </div>
         </div>
