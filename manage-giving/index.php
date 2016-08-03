01d7db007bf8af607a19ca4c461bf362ea5c4c1f
David Sudarma <david@26am.com>
2016-08-03 08:38:00 -0700

    It's something, and it's ugly.

--- manage-giving/index.php
+++ manage-giving/index.php
@@ -9,22 +9,23 @@ give_head();
 <div class="screenContent" tabindex="-1" role="dialog">
   <div class="container">
     <div class="col-md-12">
-      <div class="panel panel-margin-bottom">
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-sm-6  col-xs-12">
-
+      <div class="row">
+        <div class="col-sm-8  col-xs-12">
+          <div class="panel panel-margin-bottom">
+            <div class="panel-body">
               <!-- Default panel contents -->
               <h2 title="Staff #5564666">Jean Deaux</h2>
-
             </div>
-            <div class="col-sm-6  col-xs-12">
+          </div>
+        </div>
+        <div class="col-sm-4  col-xs-12">
+          <div class="panel panel-margin-bottom">
+            <div class="panel-body">
               <!-- Button trigger modal -->
               <button type="button" class="btn btn-primary btn-lg u-floatRight" data-toggle="modal" data-target="#myModal">
                 Give a Gift
               </button>
               <!-- Button trigger modal -->
-
             </div>
           </div>
         </div>
