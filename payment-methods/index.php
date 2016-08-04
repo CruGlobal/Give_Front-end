00d7460801a49e2c186aea902c26b6030d15eef0
Elikem Adadevoh <elikem@gmail.com>
2016-08-04 18:18:56 -0400

    Edit payment information modal

--- payment-methods/index.php
+++ payment-methods/index.php
@@ -24,8 +24,8 @@ give_head();
                     <div class="row">
                         <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                             <div class="panel panel-default panel-with-gray-background">
-                                <div class="panel-heading p" role="tab" id="headingOne">
-                                    <a class="payment-summary-link u-block" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <div class="panel-heading" role="tab" id="paymentHeading1">
+                                    <a class="payment-summary-link u-block p" role="button" data-toggle="collapse" data-parent="#accordion" href="#paymentMethod1" aria-expanded="true" aria-controls="collapseOne">
                                         <div class="row">
                                             <div class="col-md-8">
                                                 <p>
@@ -42,7 +42,7 @@ give_head();
                                     </a>
                                 </div>
 
-                                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                <div id="paymentMethod1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="paymentHeading1">
                                     <div class="panel-body">
                                         <div class="row">
                                             <div class="col-md-4">
@@ -56,7 +56,7 @@ give_head();
                                             </div>
 
                                             <div class="col-md-4 text-right">
-                                                <p><button class="btn btn-default">Edit Payment Method</button></p>
+                                                <p><a class="btn btn-default" data-toggle="modal" data-target="#editPaymentMethod">Edit Payment Method</a></p>
                                                 <p><button class="btn btn-default">Delete Payment Method</button></p>
                                             </div>
                                         </div>
@@ -66,6 +66,126 @@ give_head();
                                                 <p class="section-title mb0">Billing address</p>
                                                 <p>Samantha Bork <br> 789 Highland Meadows Parkway, Apt. 307 <br> Orlando FL 123456-7890</p>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <h4 class="section-heading pl--">Recurring Gifts Using This Payment Method:</h4>
+
+                                            <table class="table table-striped borderless">
+                                                <thead>
+                                                    <tr>
+                                                        <th class="section-title">Person or Ministry</th>
+                                                        <th class="section-title">Amount</th>
+                                                        <th class="section-title">Frequency</th>
+                                                        <th class="section-title">Next Gift</th>
+                                                        <th class="section-title"></th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Jeannette Deaux</td>
+                                                        <td>$50.00</td>
+                                                        <td>Monthly</td>
+                                                        <td>8/6/16</td>
+                                                        <td><a data-toggle="modal" data-target="#editPaymentMethod">Edit</a></td>
+                                                    </tr>
+
+                                                    <tr>
+                                                        <td>Peter Parker</td>
+                                                        <td>$100.00</td>
+                                                        <td>Quarterly</td>
+                                                        <td>7/12/16</td>
+                                                        <td><a data-toggle="modal" data-target="#editPaymentMethod">Edit</a></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default panel-with-gray-background">
+                                <div class="panel-heading" role="tab" id="paymentHeading2">
+                                    <a class="payment-summary-link u-block p" role="button" data-toggle="collapse" data-parent="#accordion" href="#paymentMethod2" aria-expanded="true" aria-controls="collapseOne">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <p>
+                                                    <span class="payment-icon"><i class="fa fa-cc-visa" aria-hidden="true"></i></span>
+                                                    <span class="payment-nickname">Visa</span>
+                                                    ending in ****1717
+                                                    <span class="payment-expiration">EXPIRES 08/2022</span>
+                                                </p>
+                                            </div>
+                                            <div class="col-md-4 text-right payment-details-link">
+                                                Details +
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div id="paymentMethod2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="paymentHeading2">
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <p class="section-title mb0">Name of card</p>
+                                                <p>Samantha H Bork</p>
+                                            </div>
+
+                                            <div class="col-md-4">
+                                                <p class="section-title mb0">Nickname</p>
+                                                <p>Sam's Visa Debit</p>
+                                            </div>
+
+                                            <div class="col-md-4 text-right">
+                                                <p><a class="btn btn-default" data-toggle="modal" data-target="#editPaymentMethod">Edit Payment Method</a></p>
+                                                <p><button class="btn btn-default">Delete Payment Method</button></p>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-12 pt">
+                                                <p class="section-title mb0">Billing address</p>
+                                                <p>Samantha Bork <br> 789 Highland Meadows Parkway, Apt. 307 <br> Orlando FL 123456-7890</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <h4 class="section-heading pl--">Recurring Gifts Using This Payment Method:</h4>
+
+                                            <table class="table table-striped borderless">
+                                                <thead>
+                                                <tr>
+                                                    <th class="section-title">Person or Ministry</th>
+                                                    <th class="section-title">Amount</th>
+                                                    <th class="section-title">Frequency</th>
+                                                    <th class="section-title">Next Gift</th>
+                                                    <th class="section-title"></th>
+                                                </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                <tr>
+                                                    <td>Jeannette Deaux</td>
+                                                    <td>$50.00</td>
+                                                    <td>Monthly</td>
+                                                    <td>8/6/16</td>
+                                                    <td><a data-toggle="modal" data-target="#editPaymentMethod">Edit</a></td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>Peter Parker</td>
+                                                    <td>$100.00</td>
+                                                    <td>Quarterly</td>
+                                                    <td>7/12/16</td>
+                                                    <td><a data-toggle="modal" data-target="#editPaymentMethod">Edit</a></td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
                                         </div>
                                     </div>
                                 </div>
@@ -82,6 +202,7 @@ give_head();
 
 <?php
 
+include('modal-edit-payment-method.html');
 give_footer();
 
 ?>
