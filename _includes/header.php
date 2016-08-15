62925730eb9318df5e682033c6d023e0b67b9c0c
David Sudarma <david@26am.com>
2016-08-15 16:40:57 -0700

    Modals Completed for Detail Editing

--- _includes/header.php
+++ _includes/header.php
@@ -13,6 +13,7 @@
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <script src="/assets/js/typekit.js"></script>
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
     <script src="/assets/bower_components/slick-carousel/slick/slick.min.js"></script>
     <script src="/assets/js/give.js"></script>
 
--- product-detail-edit/index.php
+++ product-detail-edit/index.php
@@ -19,12 +19,21 @@ give_head();
               <p class="mt0">#5552424</p>
 
             </div>
-            <div class="col-sm-6  col-xs-12">
+            <div class="col-sm-6  col-xs-12 text-right">
+              <!-- Editing Buttons -->
+              <button type="button" class="btn btn-secondary btn-lg">
+                Done Editing
+              </button>
+              <button type="button" class="btn btn-default btn-lg">
+                Edit Page
+              </button>
+              <!-- \\ Editing Buttons -->
+
               <!-- Button trigger modal -->
-              <button type="button" class="btn btn-primary btn-lg u-floatRight" data-toggle="modal" data-target="#myModal">
+              <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
                 Give a Gift
               </button>
-              <!-- Button trigger modal -->
+              <!-- \\ Button trigger modal -->
 
             </div>
           </div>
@@ -33,12 +42,18 @@ give_head();
 
       <div class="panel panel-margin-bottom">
         <div class="panel-cover-wrap">
-          <img src="http://fpoimg.com/1920x1080" class="panel-cover-image"/>
+          <a href="" data-toggle="modal" data-target="#editCover">
+            <img src="http://fpoimg.com/1920x1080" class="panel-cover-image"/>
+          </a>
         </div>
         <div class="panel-body">
           <div class="row">
             <div class="col-md-10 col-md-offset-1">
-              <img src="http://fpoimg.com/1920x1080" class="secondary-detail pull-right ml mb hidden-xs" />
+              <a href="" data-toggle="modal" data-target="#editSecondary" class=" pull-right secondary-detail ml mb hidden-xs">
+                <img src="http://fpoimg.com/1920x1080" class="" />
+              </a>
+
+              <a href="" data-toggle="modal" data-target="#editProductDesc">Edit Text</a>
               <h4>Great Spiritual Need</h4>
               <p>Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
                 dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
@@ -77,6 +92,18 @@ give_head();
 <!-- Product Config. Modal -->
 <?php include('../product-detail/product-configuration.php'); ?>
 <!-- // Product Config. Modal End -->
+
+<!-- Product Edit Cover. Modal -->
+<?php include('product-edit-cover.php'); ?>
+<!-- // Product Edit Cover. Modal End -->
+
+<!-- Product Edit Secondary. Modal -->
+<?php include('product-edit-secondary.php'); ?>
+<!-- // Product Edit Secondary. Modal End -->
+
+<!-- Product Edit Secondary. Modal -->
+<?php include('product-edit-copy.php'); ?>
+<!-- // Product Edit Secondary. Modal End -->
 
 <?php 
 
