521b93a4a3dfbb0d5732f642440463d1414c2c84
Elikem Adadevoh <elikem@gmail.com>
2016-08-17 17:25:26 -0400

    Add the rest of the missing content to the modal

--- manage-giving/modal-make-changes-manage-giving.php
+++ manage-giving/modal-make-changes-manage-giving.php
@@ -63,6 +63,55 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="row">
+                                <div class="border-bottom-small">
+                                    <div class="col-xs-3">
+                                        <img src="http://placehold.it/100x100">
+                                    </div>
+
+                                    <div class="col-xs-9">
+                                        <h3 class="mb0">Natasha Romanov</h3>
+                                        <small>#123456789</small>
+
+                                        <?php include('partial-gift-details.html'); ?>
+
+                                        <p>You have selected <strong>annually</strong> as a frequency.
+                                            Please tell us in which months you would like your annual gifts to be made
+                                        </p>
+
+                                        <div class="row">
+                                            <div class="col-xs-6">
+                                                <div class="form-group mb0">
+                                                    <select class="form-control  form-control-subtle">
+                                                        <option>January</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="border-bottom-small">
+                                    <div class="col-xs-3">
+                                        <img src="http://placehold.it/100x100">
+                                    </div>
+
+                                    <div class="col-xs-9">
+                                        <h3 class="mb0">Peter Parker</h3>
+                                        <small>#123456789</small>
+
+                                        <?php include('partial-gift-details.html'); ?>
+
+                                        <p>This gift cannot be changed online. For assistance,
+                                            please call us at (888) 278-7233 from 9am to 5pm ET, Monday - Friday, or email us at
+                                            <a href="mailto:eGift@cru.org">eGift@cru.org</a>.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
