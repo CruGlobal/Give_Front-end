dbccedba6a541af41594cc89037bb5f93b13a42d
Elikem Adadevoh <elikem@gmail.com>
2016-08-17 11:49:10 -0400

    Modal headings should be H3s.

--- manage-giving/modal-add-payment-method-manage-giving.php
+++ manage-giving/modal-add-payment-method-manage-giving.php
@@ -7,7 +7,7 @@
                         <div class="col-xs-12">
                             <div class="border-bottom-small">
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title">Add a Payment Method</h4>
+                                <h3>Add a Payment Method</h3>
                                 <small>You must have at least one valid payment method to continue. Please add one below.</small>
                             </div>
                         </div>
