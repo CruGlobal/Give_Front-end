eaf4c405f671d81e58c8ea80b191f7b78e1bd730
Elikem Adadevoh <elikem@gmail.com>
2016-09-07 19:19:39 -0400

    Navigation works per design from 4.1 - 4.3

--- mobile-nav/index.php
+++ mobile-nav/index.php
@@ -12,15 +12,26 @@ nav_mobile();
 
     <ul class="top-level-menu visible-menu-block">
       <li>How To Know God</li>
-      <li class="has-children blue"> Train + Grow
+      <li class="has-children"> Train + Grow
         <ul class="train-and-grow ">
-          <li class="back top-level-menu"><i class="fa fa-chevron-left" aria-hidden="true"></i> Train + Grow</li>
+          <li class="back top-level-menu">Train + Grow</li>
           <li class="has-children"> Spiritual Growth
             <ul class="spiritual-growth">
+              <li class="back top-level-menu">Train + Grow</li>
+              <li class="back train-and-grow">Spiritual Growth</li>
               <li>Beginning With God</li>
+              <li>Core Christian Beliefs</li>
+              <li>Devotionals + Quiet Times</li>
+              <li>Prayer</li>
+              <li>Fasting</li>
             </ul>
           </li>
           <li>Life + Relationships</li>
+          <li>Bible Studies</li>
+          <li>Share The Gospel</li>
+          <li>Help Others Grow</li>
+          <li>Leadership Training</li>
+          <li>Language Resources</li>
         </ul>
       </li>
       <li>Communities</li>
