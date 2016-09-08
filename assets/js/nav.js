bc281ce72101fe0a6045e37bcc4fc25807de5072
Elikem Adadevoh <elikem@gmail.com>
2016-09-08 12:06:11 -0400

    Fix bug - target menu not being appended to mobile navigation

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
     $("#mobile-navigation").click(function (e) {
+        var topLevelNavIsVisible = true;
         var target = e.target;
         var classes = $(target).attr('class');
         var toggleClass = "visible-menu-block";
 
-        // traverse up and down the menu
+        /*
+        traverse up and down the menu:
+        remove visible-menu-block from current menu and
+        clone target menu and append to root of mobile navigation structure
+        */
         if ((classes !== undefined) && (classes.indexOf("has-children") !== -1)) {
+            topLevelNavIsVisible = $("ul:not(:first-of-type)").length === 0;
             var targetMenuList = $(target).children("ul");
             var currentMenuList = $("." + toggleClass);
 
-            currentMenuList.toggleClass(toggleClass);
+            topLevelNavIsVisible ? currentMenuList.toggleClass(toggleClass) : currentMenuList.remove();
             $("#mobile-navigation").append(targetMenuList.clone().toggleClass(toggleClass));
         } else if ((classes !== undefined) && (classes.indexOf("back") !== -1)) {
             classes = classes.replace("back", "").trim();
@@ -18,7 +24,7 @@ $(document).ready(function () {
             currentMenuList = $("ul." + toggleClass);
 
             currentMenuList.remove();
-            targetMenuList.toggleClass(toggleClass)
+            $("#mobile-navigation").append(targetMenuList.clone().toggleClass(toggleClass));
         } else {
             target.click();
         }
