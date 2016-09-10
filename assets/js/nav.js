a6f3a72d170520659620bff90a6c0a8802fb1f21
Elikem Adadevoh <elikem@gmail.com>
2016-09-09 21:49:57 -0400

    Update navigation to 3.x

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $("#mobile-navigation").click(function (e) {
-        var topLevelNavIsVisible = true;
         var target = e.target;
         var classes = $(target).attr('class');
-        var toggleClass = "visible-menu-block";
+        var toggleClass = "visible";
+        var root = $("#mobile-navigation");
+        var mainMenu = $(".main-menu");
+        var currentMenu = $(".visible");
 
         /*
         traverse up and down the menu:
@@ -11,22 +13,48 @@ $(document).ready(function () {
         clone target menu and append to root of mobile navigation structure
         */
         if ((classes !== undefined) && (classes.indexOf("has-children") !== -1)) {
-            topLevelNavIsVisible = $("ul:not(:first-of-type)").length === 0;
-            var targetMenuList = $(target).children("ul");
-            var currentMenuList = $("." + toggleClass);
+            var targetMenu = $(target).children("ul");
 
-            topLevelNavIsVisible ? currentMenuList.toggleClass(toggleClass) : currentMenuList.remove();
-            $("#mobile-navigation").append(targetMenuList.clone().toggleClass(toggleClass));
-        } else if ((classes !== undefined) && (classes.indexOf("back") !== -1)) {
-            classes = classes.replace("back", "").trim();
+            if (isMainMenu()) {
+                $(currentMenu).toggleClass(toggleClass);
+            } else {
+                $(currentMenu).remove();
+            }
+            $(root).append(targetMenu.clone().toggleClass(toggleClass));
 
-            targetMenuList = $("ul." + classes);
-            currentMenuList = $("ul." + toggleClass);
+        } else if ((classes !== undefined) && (classes.indexOf("menu-icon-navigation") !== -1)) {
+            var targetClass = $(".visible").attr('class').replace("visible","").trim();
+            var targetMenu = $(".main-menu").find("ul." + targetClass).parent().parent();
 
-            currentMenuList.remove();
-            $("#mobile-navigation").append(targetMenuList.clone().toggleClass(toggleClass));
+            console.log(targetClass);
+            console.log(targetMenu);
+
+            if (targetMenu.attr('class') === "main-menu") {
+                $(currentMenu).remove();
+                $(mainMenu).toggleClass(toggleClass)
+            } else {
+                $(currentMenu).remove();
+                $(root).append(targetMenu.clone().toggleClass(toggleClass));
+            }
+
         } else {
             target.click();
         }
+
+        function isMainMenu () {
+            return ($(".main-menu.visible").length) ? true : false
+        }
+
+        function menuIconBackNavigation () {
+            $(".menu-icon-navigation").attr("src", "/assets/img/mobile-menu-back-icon.png");
+        }
+
+        function menuIconHamburger () {
+            $(".menu-icon-navigation").attr("src", "/assets/img/mobile-menu-back-icon.png");
+        }
+
+        $(function toggleIconBackNavigation () {
+            if (!isMainMenu()) menuIconHamburger()
+        });
     });
 });
--- mobile-nav/index.php
+++ mobile-nav/index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 require_once('../_functions/function.php');
@@ -8,38 +7,45 @@ nav_mobile();
 ?>
 
 <body>
-  <div id="mobile-navigation">
+<div id="mobile-navigation">
 
-    <ul class="top-level-menu visible-menu-block">
-      <li>How To Know God</li>
-      <li class="has-children"> Train + Grow
-        <ul class="train-and-grow ">
-          <li class="back top-level-menu">Train + Grow</li>
-          <li class="has-children"> Spiritual Growth
-            <ul class="spiritual-growth">
-              <li class="back top-level-menu">Train + Grow</li>
-              <li class="back train-and-grow">Spiritual Growth</li>
-              <li>Beginning With God</li>
-              <li>Core Christian Beliefs</li>
-              <li>Devotionals + Quiet Times</li>
-              <li>Prayer</li>
-              <li>Fasting</li>
-            </ul>
-          </li>
-          <li>Life + Relationships</li>
-          <li>Bible Studies</li>
-          <li>Share The Gospel</li>
-          <li>Help Others Grow</li>
-          <li>Leadership Training</li>
-          <li>Language Resources</li>
-        </ul>
-      </li>
-      <li>Communities</li>
-      <li>Opportunities</li>
-      <li>About</li>
-      <li>Give</li>
-    </ul>
+  <ul class="menu-icons">
+    <li><a href="#"><img class="menu-icon-navigation" src="/assets/img/mobile-menu-hamburger-icon.png"></a></li>
+    <li><a href="#"><img src="/assets/img/mobile-menu-search-icon.png"></a></li>
+    <li><a href="#"><img src="/assets/img/mobile-menu-location-icon.png"></a></li>
+    <li><a href="#"><img src="/assets/img/mobile-menu-globe-icon.png"></a></li>
+    <li><a href="#"><img src="/assets/img/mobile-menu-cart-icon.png"></a></li>
+    <li><a href="#"><img src="/assets/img/mobile-menu-ellipsis-icon.png"></a></li>
+  </ul>
 
-  </div>
+  <ul class="main-menu visible">
+    <li>How To Know God</li>
+    <li class="has-children">Train + Grow
+      <ul class="train-and-grow">
+        <li>Train + Grow</li>
+        <li class="has-children spiritual-growth">Spiritual Growth
+          <ul class="spiritual-growth">
+            <li>Spiritual Growth</li>
+            <li>Beginning With God</li>
+            <li>Core Christian Beliefs</li>
+            <li>Devotionals + Quiet Times</li>
+            <li>Prayer</li>
+            <li>Fasting</li>
+          </ul>
+        </li>
+        <li>Life + Relationships</li>
+        <li>Bible Studies</li>
+        <li>Share The Gospel</li>
+        <li>Help Others Grow</li>
+        <li>Leadership Training</li>
+        <li>Language Resources</li>
+      </ul>
+    </li>
+    <li>Communities</li>
+    <li>Opportunities</li>
+    <li>About</li>
+    <li>Give</li>
+  </ul>
+</div>
 </body>
 </html>
