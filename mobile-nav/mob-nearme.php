b85de94a94b7259bc464c35ac08704a53fefbea5
Elikem Adadevoh <elikem@gmail.com>
2016-09-15 16:22:22 -0400

    `Search` and `Find Cru Near Me` screens

--- mobile-nav/mob-nearme.php
+++ mobile-nav/mob-nearme.php
@@ -1,1 +1,5 @@
-mob-nearme.php
+<form>
+  <div class="form-group">
+    <input type="text" class="form-control form-control-subtle text-center" name="mob-nearme" placeholder="FIND CRU NEAR ME">
+  </div>
+</form>
