d8b93840a9cf0faea43f5ceb0d0d3730faa18252
Elikem Adadevoh <elikem@gmail.com>
2016-09-19 15:20:31 -0400

    Redirect search and nearme

--- mobile-nav/mob-nearme.php
+++ mobile-nav/mob-nearme.php
@@ -1,5 +1,14 @@
-<form>
+<form id="mob-nearme">
   <div class="form-group">
     <input type="text" class="form-control form-control-subtle text-center" name="mob-nearme" placeholder="FIND CRU NEAR ME">
   </div>
 </form>
+
+<script>
+  $("form#mob-nearme").submit(function (e) {
+    e.preventDefault();
+
+    var query = $(this).find("input").val();
+    window.location.href ="https://www.cru.org/locator/colleges.html?q=" + query;
+  })
+</script>
