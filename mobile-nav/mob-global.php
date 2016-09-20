fb4c860012d73c238509889931bb1add4a623cf7
Elikem Adadevoh <elikem@gmail.com>
2016-09-20 18:42:26 -0400

    Add placeholder for global tab

--- mobile-nav/mob-global.php
+++ mobile-nav/mob-global.php
@@ -1,36 +1,28 @@
 <div class="main-menu-container">
   <ul class="main-menu main-menu-global">
-    <li><a href="#">Africa</a></li>
-    <li class="has-children">Train + Grow
+    <li class="has-children">Africa
       <ul>
-        <li><a href="#">Train + Grow</a></li>
-        <li class="has-children">Spiritual Growth
-          <ul>
-            <li><a href="#">Spiritual Growth</a></li>
-            <li><a href="#">Beginning With God</a></li>
-            <li><a href="#">Core Christian Beliefs</a></li>
-            <li><a href="#">Devotionals + Quiet Times</a></li>
-            <li><a href="#">Prayer</a></li>
-            <li><a href="#">Fasting</a></li>
-          </ul>
-        </li>
-        <li>Life + Relationships</li>
-        <li>Bible Studies</li>
-        <li class="has-children">Share The Gospel
-          <ul>
-            <li><a href="#">Share the Gospel</a></li>
-            <li><a href="#">Digital Ministry</a></li>
-            <li><a href="#">Evangelism Principles</a></li>
-          </ul>
-        </li>
-        <li>Help Others Grow</li>
-        <li>Leadership Training</li>
-        <li>Language Resources</li>
+        <li><a href="#">Ethiopia</a></li>
+        <li><a href="#">Ghana</a></li>
+        <li><a href="#">Kenya</a></li>
+        <li><a href="#">Nigeria</a></li>
+        <li><a href="#">Nigeria</a></li>
       </ul>
     </li>
-    <li>Communities</li>
-    <li>Opportunities</li>
-    <li>About</li>
-    <li>Give</li>
+    <li class="has-children">Asia
+      <ul>
+        <li><a href="#">Armenia</a></li>
+        <li><a href="#">Hong Kong</a></li>
+        <li><a href="#">India</a></li>
+        <li><a href="#">Japan</a></li>
+        <li><a href="#">Macau</a></li>
+        <li><a href="#">Nepal</a></li>
+        <li><a href="#">Pakistan</a></li>
+        <li><a href="#">Singapore</a></li>
+        <li><a href="#">South Korea</a></li>
+        <li><a href="#">Taiwan</a></li>
+        <li><a href="#">Thailand</a></li>
+      </ul>
+    </li>
   </ul>
 </div>
