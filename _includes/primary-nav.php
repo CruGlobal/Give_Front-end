ed5a77b4480448b47b6317ea80949ed6202d247e
Elikem Adadevoh <elikem@gmail.com>
2016-10-01 18:41:10 -0400

    Add sub nav

--- _includes/primary-nav.php
+++ _includes/primary-nav.php
@@ -73,27 +73,24 @@
 
 </header>
 
-<div id="desktop-sub-navigation">
-  <div class="container">
-    <div class="breadcrumbs list-inline">
-      <ul>
-        <li>Cities</li>
-        <li>Portland</li>
-      </ul>
-    </div>
+<div id="sub-navigation">
+  <ul class="breadcrumbs">
+    <li><a href="#">Cities</a></li>
+    <li><a href="#">Portland</a></li>
+  </ul>
 
-    <div class="sub-navigation list-inline">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Events</li>
-        <li>Small Groups</li>
-        <li>Contact</li>
-        <li>Join</li>
-      </ul>
-    </div>
-  </div>
+  <ul class="sub-nav">
+    <li><a href="#" class="active">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Events</a></li>
+    <li><a href="#">Small Groups</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Join</a></li>
+  </ul>
 </div>
+
+
+
 
 
 <script>
