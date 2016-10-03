637f5a173ee2c58afe29a63493158b53ee6002c5
Elikem Adadevoh <elikem@gmail.com>
2016-10-03 11:11:25 -0400

    Add logo to sub-nav

--- _includes/primary-nav.php
+++ _includes/primary-nav.php
@@ -74,19 +74,23 @@
 </header>
 
 <div id="sub-navigation">
-  <ul class="breadcrumbs">
-    <li><a href="#">Cities</a></li>
-    <li><a href="#">Portland</a></li>
-  </ul>
+  <div class="container">
+    <a href="" title="Cru" class="sub-nav-logo"></a>
 
-  <ul class="sub-nav">
-    <li><a href="#" class="active">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Events</a></li>
-    <li><a href="#">Small Groups</a></li>
-    <li><a href="#">Contact</a></li>
-    <li><a href="#">Join</a></li>
-  </ul>
+    <ul class="breadcrumbs">
+      <li><a href="#">Cities</a></li>
+      <li><a href="#">Portland</a></li>
+    </ul>
+
+    <ul class="sub-nav">
+      <li><a href="#" class="active">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Events</a></li>
+      <li><a href="#">Small Groups</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Join</a></li>
+    </ul>
+  </div>
 </div>
 
 
