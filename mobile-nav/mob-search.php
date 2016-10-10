d5f079402a7ecf9baac17333a20599fd5ce52a70
David Sudarma <david@26am.com>
2016-10-10 08:17:20 -0400

    Cleaned up Clashing IDs and styles Cleaned up DIV sizing and Anchor tag use for Mobile More Nav.

--- mobile-nav/mob-search.php
+++ mobile-nav/mob-search.php
@@ -1,4 +1,4 @@
-<form id="mob-search">
+<form id="mob-search-form">
   <div class="form-group">
     <input type="text" class="form-control form-control-subtle text-center" name="mob-search" placeholder="ENTER KEYWORDS">
   </div>
