1d96784b8af12d79cf52c6dfa52bf207b73fcb40
Elikem Adadevoh <elikem@gmail.com>
2016-10-12 09:21:31 -0400

    Add new payment method to one-time gift

--- manage-giving/modal-select-method.php
+++ manage-giving/modal-select-method.php
@@ -8,7 +8,7 @@
                             <div class="border-bottom-small">
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                 <h3>Give an Extra One-Time Gift</h3>
-                                <p>How much would you like to give to these recipients?</p>
+                                <p>Select your payment method</p>
                             </div>
                         </div>
                     </div>
@@ -20,16 +20,25 @@
                     <div class="row">
                         <div class="col-xs-12">
                           <div class="repeating-row select-recipient-row">
-                            <?php 
-                            $i = 1;
-                            while($i <= 3) { ?>
+                              <form id="select-payment-method">
+                                  <?php
+                                  $i = 1;
+                                  while($i <= 3) { ?>
 
-                                <?php include('../checkout-payment-method/partial-existing-method-row.html'); ?>
+                                      <?php include('../checkout-payment-method/partial-existing-method-row.html'); ?>
 
-                            <?php 
-                            $i++; 
-                            } 
-                            ?>
+                                      <?php
+                                      $i++;
+                                  }
+                                  ?>
+
+                                  <div class="radio radio-method">
+                                      <label>
+                                          <input type="radio" name="chosen_method" id="" value="AddNewCardOrBankAccount">
+                                          Add New Credit Card or Bank Account
+                                      </label>
+                                  </div>
+                              </form>
 
                             <div class="mt-">
                               <p class="inline-block">Total for This Gift:</p>
@@ -48,10 +57,24 @@
                         <a class="btn btn-default" data-dismiss="modal" data-toggle="modal" data-target="#configGiftAmount">Back</a>
                     </div>
                     <div class="col-xs-6 text-right">
-                        <a class="btn btn-primary" href="../thank-you/index.php">Continue</a>
+                        <a class="btn btn-primary" href="../thank-you/index.php" onclick="$('#select-payment-method').submit(); return false;">Continue</a>
+                        <a class="hidden go-to-create-new-account" data-dismiss="modal" data-toggle="modal" data-target="#addPaymentMethodManageGiving">Continue</a>
+                        <a class="hidden go-to-confirm-one-time-amount" data-dismiss="modal" data-toggle="modal" data-target="#confirmExtraOneTimeManageGiving">Continue</a>
                     </div>
                 </div>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
+
+<script>
+    $('#select-payment-method').submit(function () {
+        if ($("input[value=AddNewCardOrBankAccount]").prop("checked")) {
+            $(".go-to-create-new-account").click();
+        } else {
+            $(".go-to-confirm-one-time-amount").click();
+        }
+
+        return false;
+    })
+</script>
