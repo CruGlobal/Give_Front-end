670a5db838cf0e2987905142f1d7ba4388608a68
David Sudarma <david@26am.com>
2016-10-13 12:30:45 -0400

    Applied receipt updates from Master into Dev Branch

--- manage-giving/receipts.php
+++ manage-giving/receipts.php
@@ -8,101 +8,108 @@ give_head();
 
   <div class="screenContent" tabindex="-1" role="dialog">
     <div class="container">
-      <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-12">
 
-        <div class="panel panel-margin-bottom">
-          <div class="panel-body">
-            <div class="row">
-              <div class="col-md-12">
-                <h3>Receipts</h3>
+          <div class="panel panel-margin-bottom">
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-12">
+                  <h3>Receipts</h3>
 
-                <form class="form-inline mt">
-                  <div class="form-group">
-                    <span class="pr-">View My Gifts:</span>
-                  </div>
-
-                  <div class="input-group">
-                    <div class="input-group-btn">
-                      <button type="button" class="btn btn-default ps_x active">2016</button>
-                      <button type="button" class="btn btn-default ps_x">2015</button>
+                  <form class="form-inline mt">
+                    <div class="form-group">
+                      <span class="pr-">View My Gifts:</span>
                     </div>
-                  </div>
 
-                  <div class="form-group">
-                    <a href="" class="pl- u-textWeight-semiBold">More History</a>
-                  </div>
-                </form>
+                    <div class="input-group">
+                      <div class="btn-group">
+                        <button type="button"
+                              class="btn btn-default dropdown-toggle"
+                              data-toggle="dropdown"
+                              aria-haspopup="true"
+                              aria-expanded="false">
+                            <span class="hidden-xs">2016 <span class="caret"></span></span>
+                            <span class="visible-xs"><span class="fa fa-filter"></span></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">2016</a></li>
+                            <li><a href="#">2015</a></li>
+                            <li><a href="#">2014</a></li>
+                            <li><a href="#">2013</a></li>
+                            <li><a href="#">2012</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-            <div class="row mt">
-              <div class="col-md-12">
-                <div class="table-responsive">
-                  <table class="table table-striped table-striped-dark borderless">
-                    <thead>
-                    <tr>
-                      <td>Receipt Date</td>
-                      <td>Amount</td>
-                      <td>Gift Details</td>
-                      <td></td>
-                    </tr>
-                    </thead>
+              <div class="row mt">
+                <div class="col-md-12">
+                  <div class="table-responsive">
+                    <table class="table table-striped table-payment-history">
+                      <thead>
+                      <tr>
+                        <th>Receipt Date</th>
+                        <th>Amount</th>
+                        <th>Gift Details</th>
+                        <th></th>
+                      </tr>
+                      </thead>
 
-                    <tbody>
-                    <tr>
-                      <td>July 6, 2016</td>
-                      <td>$225.00</td>
-                      <td>Bruce Banner, Peter Parker, Jeanette Deaux, Clare Angus</td>
-                      <td>
-                        <p class="mb0">#123456789</p>
-                        <a class="u-textWeight-semiBold">View PDF</a>
-                      </td>
-                    </tr>
+                      <tbody>
+                      <tr>
+                        <td>July 6, 2016</td>
+                        <td>$225.00</td>
+                        <td>Bruce Banner, Peter Parker, Jeanette Deaux, Clare Angus</td>
+                        <td>
+                          <a class="u-textWeight-semiBold">View PDF</a>
+                        </td>
+                      </tr>
 
-                    <tr>
-                      <td>July 1, 2016</td>
-                      <td>$50.00</td>
-                      <td>Jeannette Deaux</td>
-                      <td>
-                        <p class="mb0">#123456789</p>
-                        <a class="u-textWeight-semiBold">View PDF</a>
-                      </td>
-                    </tr>
+                      <tr>
+                        <td>July 1, 2016</td>
+                        <td>$50.00</td>
+                        <td>Jeannette Deaux</td>
+                        <td>
+                          <a class="u-textWeight-semiBold">View PDF</a>
+                        </td>
+                      </tr>
 
-                    <tr>
-                      <td>June 28, 2016</td>
-                      <td>$25.00</td>
-                      <td>Bruce Banner</td>
-                      <td>
-                        <p class="mb0">#123456789</p>
-                        <a class="u-textWeight-semiBold">View PDF</a>
-                      </td>
-                    </tr>
+                      <tr>
+                        <td>June 28, 2016</td>
+                        <td>$25.00</td>
+                        <td>Bruce Banner</td>
+                        <td>
+                          <a class="u-textWeight-semiBold">View PDF</a>
+                        </td>
+                      </tr>
 
-                    <tr>
-                      <td>July 1, 2016</td>
-                      <td>$100.00</td>
-                      <td>Peter Parker</td>
-                      <td>
-                        <p class="mb0">#123456789</p>
-                        <a class="u-textWeight-semiBold">View PDF</a>
-                      </td>
-                    </tr>
-                    </tbody>
-                  </table>
+                      <tr>
+                        <td>July 1, 2016</td>
+                        <td>$100.00</td>
+                        <td>Peter Parker</td>
+                        <td>
+                          <a class="u-textWeight-semiBold">View PDF</a>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row mt">
+                <div class="col-xs-12">
+                  <button class="btn btn-default center-block ps_x">Load More</button>
                 </div>
               </div>
             </div>
-
-            <div class="row mt">
-              <div class="col-xs-12">
-                <button class="btn btn-default center-block ps_x">Load More</button>
-              </div>
-            </div>
           </div>
+
+          <?php include('../_includes/search.php'); ?>
+
         </div>
-
-        <?php include('../_includes/search.php'); ?>
-
       </div>
     </div>
   </div>
